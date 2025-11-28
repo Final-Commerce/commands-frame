766d718abbe0253aee38d793c2def5064ae1e2f3
alex-zhezh <oz@finalpos.com>
2025-11-28 08:44:05 -0330

    chore: add dev dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:react-hooks/recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime"
+    ],
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react-refresh"],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: __dirname
+    },
+    rules: {
+        "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+        "@typescript-eslint/no-unsafe-assignment": "off"
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    }
+};
